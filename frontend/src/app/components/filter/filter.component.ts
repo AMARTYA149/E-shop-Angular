1c97ac47c276d1f983cacf2d1b67df026eacdcdf
Amartya Aishwarya <amartya970914@gmail.com>
2021-07-08 16:54:30 +0530

    fetching category

--- frontend/src/app/components/filter/filter.component.ts
+++ frontend/src/app/components/filter/filter.component.ts
@@ -1,15 +1,34 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category/category.service';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  styleUrls: ['./filter.component.scss'],
 })
 export class FilterComponent implements OnInit {
-
-  constructor() { }
+  categories: Category[] = [];
+  constructor(private _categoryService: CategoryService) {}
 
   ngOnInit(): void {
+    this.collectAllCategory();
   }
 
+  collectAllCategory() {
+    this._categoryService.getAllCategories().subscribe({
+      next: (categories) => {
+        this.categories = categories;
+        console.log(this.categories);
+      },
+      error: (response: HttpErrorResponse) => {
+        console.log(response);
+      },
+    });
+  }
+
+  categorySelected(category_id: string) {
+    console.log(category_id);
+  }
 }
--- frontend/src/app/services/category/category.service.ts
+++ frontend/src/app/services/category/category.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Category } from 'src/app/models/category';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
+  getUrl = 'http://localhost/api/categories';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAllCategories() {
+    return this.http.get(this.getUrl).pipe(
+      map((result) => {
+        return <Category[]>result['categories'];
+      })
+    );
+  }
 }
