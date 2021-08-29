d53ab553b9c0ada2d6210f900f03b55474e2f833
AmartyaBOL <amartya.aishwarya@lji.io>
2021-08-29 21:19:02 +0530

    create product - ctegory form

--- frontend/src/app/components/admin/admin-new-product/admin-new-product.component.ts
+++ frontend/src/app/components/admin/admin-new-product/admin-new-product.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-new-product',
   templateUrl: './admin-new-product.component.html',
-  styleUrls: ['./admin-new-product.component.scss']
+  styleUrls: ['./admin-new-product.component.scss'],
 })
 export class AdminNewProductComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  saveCategory(form: FormGroup) {}
 
+  saveProduct(form: FormGroup) {}
 }
