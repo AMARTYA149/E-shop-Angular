4213266d512fd9d30cce0ed9eff42b7bb9699378
AmartyaBOL <amartya.aishwarya@lji.io>
2021-09-05 18:39:54 +0530

    products listing for admin

--- frontend/src/app/components/admin/admin-products/admin-products.component.ts
+++ frontend/src/app/components/admin/admin-products/admin-products.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
-  styleUrls: ['./admin-products.component.scss']
+  styleUrls: ['./admin-products.component.scss'],
 })
 export class AdminProductsComponent implements OnInit {
-
-  constructor() { }
+  products: Product[] = [];
+  constructor(private _productService: ProductService) {}
 
   ngOnInit(): void {
+    this.collectAllProducts();
   }
 
+  collectAllProducts() {
+    this._productService.getAllProducts({}).subscribe({
+      next: (res) => {
+        this.products = res;
+      },
+    });
+  }
 }
